5561e4f0ca9ebb9dfea5b17062c9848e9b1d62d6
abraham-d <abrahamd@hotmail.com>
2017-04-13 22:15:47 -0400

    initial checkin

---
+++ basics-assignment-1-start/src/app/successalert/successalert.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-successalert',
+    template: `
+        <p>SuccessAlert works!</p>
+    `,
+    styles: [`
+        p {
+            padding: 10px;
+            border: 1px darkgreen;
+            background-color: lightgreen;
+            color: darkgreen;
+        }
+    `]
+})
+
+export class SuccessAlertComponent {
+
+}
