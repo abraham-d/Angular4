0b63f84ce8e250afdc8a5bb5d331387cc5dac603
abraham-d <abrahamd@hotmail.com>
2017-04-15 12:26:01 -0400

    more code

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showMessage = false;
+  // counter = 0;
+  messages = [];
+
+  onBtnClick() {
+    this.showMessage = !this.showMessage;
+    // this.counter = ++this.counter;
+    // this.messages.push(this.counter);
+    // this.messages.push(this.messages.length + 1);
+    this.messages.push(new Date());
+  }
 }
